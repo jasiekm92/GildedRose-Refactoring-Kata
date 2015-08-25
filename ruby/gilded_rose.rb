ce103bbcb5b1ed3539b1815f43fcada7144ff909
ArturT <artur.trzop@lunarlogic.io>
2015-08-25 15:59:52 +0200

    first refactor

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,54 +1,98 @@
 class GildedRose
 
+
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+  def update_item(item)
+
+    if item.name == "Aged Brie"
         item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
+      if item.quality < 50
+        if item.sell_in < 0
+          item.quality = item.quality + 2
         else
-          if item.quality < 50
+          item.quality = item.quality + 1
+        end
+      end
+    elsif item.name == "Sulfuras, Hand of Ragnaros"
+
+    elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+      item.sell_in = item.sell_in - 1
+      if item.quality < 50 && item.quality > 0
+        item.quality = item.quality + 1
+        if item.sell_in < 0
+          item.quality = 0
+        elsif item.sell_in < 10 && item.quality < 49
+          item.quality = item.quality + 1
+          if item.sell_in < 5 && item.quality < 48
             item.quality = item.quality + 1
           end
         end
       end
+    else
+      item.sell_in = item.sell_in - 1
+      if item.quality < 50 && item.quality > 0
+        item.quality = item.quality - 1
+
+        if item.sell_in < 0
+          item.quality = item.quality - 1
+        end
+      end
+
+
+      # if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+      #   if item.quality > 0
+      #     if item.name != "Sulfuras, Hand of Ragnaros"
+      #       item.quality = item.quality - 1
+      #     end
+      #   end
+      # else
+      #   if item.quality < 50
+      #     item.quality = item.quality + 1
+      #     if item.name == "Backstage passes to a TAFKAL80ETC concert"
+      #       if item.sell_in < 11
+      #         if item.quality < 50
+      #           item.quality = item.quality + 1
+      #         end
+      #       end
+      #       if item.sell_in < 6
+      #         if item.quality < 50
+      #           item.quality = item.quality + 1
+      #         end
+      #       end
+      #     end
+      #   end
+      # end
+
+      # if item.name != "Sulfuras, Hand of Ragnaros"
+      #   item.sell_in = item.sell_in - 1
+      # end
+
+      # if item.sell_in < 0
+      #   if item.name != "Aged Brie"
+      #     if item.name != "Backstage passes to a TAFKAL80ETC concert"
+      #       if item.quality > 0
+      #         if item.name != "Sulfuras, Hand of Ragnaros"
+      #           item.quality = item.quality - 1
+      #         end
+      #       end
+      #     else
+      #       item.quality = item.quality - item.quality
+      #     end
+      #   else
+      #     if item.quality < 50
+      #       item.quality = item.quality + 1
+      #     end
+      #   end
+      # end
+    end
+  end
+
+  def update_quality()
+    @items.each do |item|
+      update_item(item)
     end
   end
 end
